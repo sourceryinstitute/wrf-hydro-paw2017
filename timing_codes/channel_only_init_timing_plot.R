6faddf5d8435bf22ecb053419561b5fe5135a22a
James L McCreight <mccreigh@gmail.com>
2017-08-16 00:56:04 -0600

    one small save

--- timing_codes/channel_only_init_timing_plot.R
+++ timing_codes/channel_only_init_timing_plot.R
@@ -6,11 +6,13 @@ library(ggplot2)
 
 options(warn=1)
 
+baseSize=13
+
 ggplot(timing) +
     geom_point(aes(x=nCores, y=seconds)) +
     scale_x_continuous(trans='log2', breaks=timing$nCores, name='# of cores') +
     scale_y_continuous(trans='log2', breaks=c(timing$nCores)) +
-    theme_bw(base_size=16) +
+    theme_bw(base_size=baseSize) +
     ggtitle('WRF-Hydro Initialization (Channel-only mode)')
 ggsave('~/WRF_Hydro/wrf-hydro-paw2017/figures/init_timing_log.png')
 
@@ -18,6 +20,6 @@ ggplot(timing) +
    geom_point(aes(x=nCores, y=seconds)) +
     scale_x_continuous(breaks=timing$nCores, name='# of cores') +
     scale_y_continuous(breaks=c(timing$nCores)) +
-    theme_bw(base_size=13) +
+    theme_bw(base_size=baseSize) +
     ggtitle('WRF-Hydro Initialization (Channel-only mode)')
 ggsave('~/WRF_Hydro/wrf-hydro-paw2017/figures/init_timing_linear.png')
