bbcf99347144b75960f74588359205416a9a446c
Damian Rouson <damian@sourceryinstitute.org>
2017-12-15 14:52:30 -0800

    Move assert util to 1-column fig; inline function

--- figures/tests/get-communicator.f90
+++ figures/tests/get-communicator.f90
@@ -1,66 +1,35 @@
-module assertions_module
-  implicit none
-contains
-  elemental subroutine assert(assertion,description)
-    logical, intent(in) :: assertion
-    character(len=*), intent(in), optional :: description
-    integer, parameter :: max_digits=12
-    character(len=max_digits) :: image_number
-    if (.not.assertion) then
-      write(image_number,*) this_image()
-      error stop "Assertion '" // description // "' failed on image " // trim(image_number)
-    end if
-  end subroutine
-end module
-
 program main  !! Test get_communicator language extension
   use opencoarrays, only : get_communicator
   use assertions_module, only : assert
+  use iso_fortran_env, only : team_type
+  use opencoarrays, only : get_communicator
+  type(team_type) :: league
+  integer, parameter :: num_teams=2 !! number of child teams to form
   implicit none
-
   call mpi_matches_caf(get_communicator()) !! verify rank & image numbering
-  block
-    use iso_fortran_env, only : team_type
-    use opencoarrays, only : get_communicator
-    type(team_type) :: league
-    integer, parameter :: num_teams=2 !! number of child teams to form
-
-    associate(initial_image=>this_image(), initial_num_images=>num_images(), chosen_team=>destination_team(this_image(),num_teams))
-      form team(chosen_team,league) !! create mapping
-      change team(league) !! join child team
-        call mpi_matches_caf(get_communicator()) !! verify new rank/image numbers
-        associate(my_team=>team_number())
-          call assert(my_team==chosen_team,"assigned team matches chosen team")
-          associate(new_num_images=>initial_num_images/num_teams+merge(1,0,my_team<=mod(initial_num_images,num_teams)))
-           call assert(num_images()==new_num_images,"block distribution of images")
-          end associate
-        end associate
-      end team
-      call assert( initial_image==this_image(),"correctly remapped to original image number")
-      call assert( initial_num_images==num_images(),"correctly remapped to original number of images")
-    end associate
-  end block
-  sync all
-  if (this_image()==1) print *,"Test passed."
+  associate(initial_image=>this_image(), initial_num_images=>num_images(), destination_team=>mod(this_image()+1,num_teams)+1)
+    form team(destination_team,league) !! create mapping
+    change team(league) !! join child team
+      call mpi_matches_caf(get_communicator()) !! verify new rank/image numbers
+      associate(my_team=>team_number())
+        call assert(my_team==destination_team,"assigned team matches chosen team")
+        associate(new_num_images=>initial_num_images/num_teams+merge(1,0,my_team<=mod(initial_num_images,num_teams)))
+         call assert(num_images()==new_num_images,"block distribution of images")
+      end associate; end associate
+    end team
+    call assert( initial_image==this_image(),"correctly remapped to original image number")
+    call assert( initial_num_images==num_images(),"correctly remapped to original number of images")
+  end associate
+  sync all; if (this_image()==1) print *,"Test passed."
 contains
-   pure function destination_team(image,numTeams) result(team)
-     integer, intent(in) ::image, numTeams
-     integer ::team
-     team = mod(image+1,numTeams)+1
-   end function
-
-  subroutine mpi_matches_caf(comm)
-    !! verify new # ranks = new # images & new image number = new rank + 1
+  subroutine mpi_matches_caf(comm) !! verify num. ranks =  num. images & image num. = rank num. + 1
     use iso_c_binding, only : c_int
     use mpi, only : MPI_COMM_SIZE, MPI_COMM_RANK
-    integer(c_int), intent(in) :: comm
-      !! MPI communicator
+    integer(c_int), intent(in) :: comm  !! MPI communicator
     integer(c_int) :: isize,ierror,irank
     call MPI_COMM_SIZE(comm, isize, ierror)
-    call assert( ierror==0 , "'call MPI_COMM_SIZE' successful" )
-    call assert( isize==num_images(), "num MPI ranks = num CAF images " )
+    call assert( [ierror==0, isize==num_images()] ,"correct rank/image cardinality" )
     call MPI_COMM_RANK(comm, irank, ierror)
-    call assert( ierror==0 , "'call MPI_COMM_RANK' successful" )
-    call assert( irank==this_image()-1 , "correct rank/image correspondence" )
+    call assert( [ierror==0, irank==this_image()-1,"correct rank/image numbering correspondence" )
   end subroutine
 end program
--- figures/tests/team-number.f90
+++ figures/tests/team-number.f90
@@ -1,5 +1,19 @@
-program main
- !! Test team_number intrinsic function
+module assertions_module!Terminate globally if test fails
+ implicit none
+contains
+ elemental subroutine assert(assertion,description)
+   logical, intent(in) :: assertion
+   character(len=*), intent(in), optional :: description
+   integer, parameter :: max_digits=12
+   character(len=max_digits) :: image_number
+   if (.not.assertion) then
+    write(image_number,*) this_image()
+    error stop description//" failed on image "//image_number
+   end if
+ end subroutine
+end module
+
+program main !! Test team_number intrinsic function
  use iso_fortran_env, only : team_type
  use assertions_module, only : assertions
  implicit none
@@ -7,13 +21,11 @@ program main
  type(team_type), target :: home
  call assert(team_number()==standard_initial_value)
  associate(my_team=>mod(this_image(),2)+1)
-  form team(my_team,home)
-    !! Map image numbers: my_team=1 for even, 2 for odd
+  form team(my_team,home)!Map even|odd images->teams 1|2
   change team(home)
     call assert(team_number()==my_team,"correct mapping")
   end team
   call assert(team_number()==standard_initial_value)
  end associate
- sync all
- if (this_image()==1) print *,"Test passed."
+ sync all; if (this_image()==1) print *,"Test passed."
 end program
